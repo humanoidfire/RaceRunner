04e5d5942c4e80f1222cb30f402338dce7b0c4cb
humanoidfire <60526001+humanoidfire@users.noreply.github.com>
2020-05-20 22:00:25 -0400

    Update Config.swift
    Add api keys for google maps and pubnub

--- RaceRunner/Config.swift
+++ RaceRunner/Config.swift
@@ -10,9 +10,9 @@ import Foundation
 
 struct Config {
   static let darkSkyKey = "51e9be2653fc075c805c92db32a50432"
-  static let googleMapsKey = "AIzaSyCYphUv_GzYX-OIWqn77u6Cpa51e1rJ2Rs"
-  static let pubNubPublishKey = "pub-c-0d826075-d0b1-4788-a7b6-40cdffc0414d"
-  static let pubNubSubscribeKey = "sub-c-8da5fcec-8aee-11e5-a04a-0619f8945a4f"
+  static let googleMapsKey = "AIzaSyC0jDVw4y3Cbrs1CHH9VuWYf5j00j8eaeY" //"AIzaSyCYphUv_GzYX-OIWqn77u6Cpa51e1rJ2Rs"
+  static let pubNubPublishKey = "pub-c-d93637bc-8f55-4300-a0cb-098ec61783da" // "pub-c-0d826075-d0b1-4788-a7b6-40cdffc0414d"
+  static let pubNubSubscribeKey = "sub-c-a55f7340-9b06-11ea-8e2f-c62edd1c297d" //"sub-c-8da5fcec-8aee-11e5-a04a-0619f8945a4f"
 
   static func checkKeys() {
     if Config.googleMapsKey == "" {
